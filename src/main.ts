ae81f8757bf7ed928bbb494878fce6546cb5d482
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2024-12-18 12:05:50 +0100

    🚀added so the quiz starts over with 10 other questions and when game is over after two rounds the game clears the questions

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,35 @@ import { Question } from "./questions.mts";
 // Variabel for questions
 let currentQuestionIndex = 0;
 let selectedQuestions: Question[] = [];
+const usedQuestions: Set<Question> = new Set();
 
 // Function for randomize questions
 function selectRandomQuestions(): Question[] {
-  const shuffled = quizQuestions.sort(() => Math.random() - 0.5);
-  return shuffled.slice(0, 10);
+    const availableQuestions = quizQuestions.filter((q) => !usedQuestions.has(q));
+  
+    // If the user played two times the quiz starts over
+    if (availableQuestions.length < 10) {
+      usedQuestions.clear(); 
+      availableQuestions.push(...quizQuestions);
+    }
+  
+    const shuffled = availableQuestions.sort(() => Math.random() - 0.5);
+    const questions = shuffled.slice(0, 10);
+  
+    questions.forEach((q) => usedQuestions.add(q)); 
+    return questions;
 }
 
 // Function for display a question
 function displayQuestion(): void {
   if (currentQuestionIndex >= selectedQuestions.length) {
-    document.getElementById('nextQuestion')!.setAttribute('disabled', 'true');
+    document.getElementById("nextQuestion")!.setAttribute("disabled", "true");
     return;
   }
   
   const question = selectedQuestions[currentQuestionIndex];
-  document.getElementById('questionTitle')!.textContent = `Fråga nr ${currentQuestionIndex + 1}`;
-  document.getElementById('question')!.innerHTML = `
+  document.getElementById("questionTitle")!.textContent = `Fråga nr ${currentQuestionIndex + 1}`;
+  document.getElementById("question")!.innerHTML = `
     ${question.question}
   `;
 }
@@ -32,17 +44,24 @@ function handleNextQuestion(): void {
   displayQuestion();
 }
 
-//When start quiz is clicked the questions display
+// When start quiz is clicked the questions display
 function startQuiz(): void {
     selectedQuestions = selectRandomQuestions();
     currentQuestionIndex = 0;
 
     displayQuestion();
 
-    document.getElementById('nextQuestion')!.addEventListener('click', handleNextQuestion);
+    document.getElementById("nextQuestion")!.addEventListener("click", handleNextQuestion);
 }
+
+// Start over the quiz
+function handlePlayAgain(): void {
+    document.getElementById("nextQuestion")!.removeAttribute("disabled");
+    startQuiz();
+  }
   
 // When the page is loaded
-document.addEventListener('DOMContentLoaded', () => {
-document.getElementById('startQuiz')!.addEventListener('click', startQuiz);
+document.addEventListener("DOMContentLoaded", () => {
+document.getElementById("startQuiz")!.addEventListener("click", startQuiz);
+document.getElementById("playAgain")!.addEventListener("click", handlePlayAgain);
 });

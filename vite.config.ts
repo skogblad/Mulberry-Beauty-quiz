9c5972c3f6c650e81a3755b2b477b7167ffc0fdf
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2024-12-20 09:45:45 +0100

    📝Deleted comment in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/fed24d-js-intro-inl-2-quiz-mulberry-beauty/", // TODO: Ändra till ert reponamn här
+  base: "/fed24d-js-intro-inl-2-quiz-mulberry-beauty/", 
 });
